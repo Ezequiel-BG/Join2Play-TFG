32d15ebd8ae326d42afc52f3b7d96722a60ea864
Ezequiel <barrosogonzalezezequiel@gmail.com>
2025-06-05 13:38:01 +0200

    errores de despliegue

--- Laravel_API/routes/api.php
+++ Laravel_API/routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
-use App\Http\Controllers\api\UsersController;
-use App\Http\Controllers\api\VideogameController;
+use App\Http\Controllers\Api\UsersController;
+use App\Http\Controllers\Api\VideogameController;
 use Illuminate\Support\Facades\Route;
 
 //Rutas para el auth
